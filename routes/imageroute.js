ab14cf45228a2b49629b60b037d1cd5ec30188b8
spartango@gmail.com <spartango@gmail.com>
2011-12-26 23:35:47 -0800

    intermediate

--- routes/imageroute.js
+++ routes/imageroute.js
@@ -1,3 +1,8 @@
+// TODO fix imports...we need fs and Image, Roi and Tag
+
+var TILE_WIDTH  = 256;
+var TILE_LENGTH = 256;
+
 /*
  * GET a raw image
  */
@@ -6,19 +11,36 @@ exports.image = function(req, res){
     // Get the image ID
     var imageId = req.params.id;
     
-    if(imageId != null) {
+    if(imageId) {
         Image.get(imageId).on('success', function(image) {
-            if(image != null) {
-                var filename = image.filename;
+            if(image) {
                 // Get the raw file from the disk
+                fs.readFile(image.filename, 
+                    function(err, data) {
+                        if(err) {
+                            // Error Condition
+                            res.send('', 404);
+                        } else {
+                            res.writeHead(200, {'Content-Type': 'image/tiff' });
+                            res.end(img, 'binary');
+                        }
+                    }
+                );
                 // Send it along
+            } else {
+                // Error condition
+                // Send a 404 back
+                res.send('', 404);
             }
 
         });  
-    } 
+    } else {
+        // param Error condition
+        // Send a 400 back
+        res.send('', 400);
+    }
 
-    // Error condition
-    // Send a 404 back
+
 };
 
 /*
@@ -28,26 +50,42 @@ exports.image = function(req, res){
 exports.tile = function(req, res){
     // Get the image ID
     var imageId = req.params.id;
-    var xOffset = req.params.x;
-    var yOffset = req.params.y;
+    var xOffset = TILE_WIDTH * Math.floor(req.params.x / TILE_WIDTH);
+    var yOffset = TILE_LENGTH * Math.floor(req.params.y / TILE_LENGTH);
     // Assert that the image offsets are safe
     // Floor the image offsets to the nearest bin 
 
-    if(imageId != null && xOffset != null && yOffset != null) {
+    if(imageId && xOffset && yOffset) {
         Image.get(imageId).on('success', function(image) {
-            if(image != null) {
-                var filename = image.filename;
+            if(image) {
+                var tilename = tiling.generateTileName(xOffset, yOffset, image.filename);
 
-                // Generate the tilename
                 // Get the tile from disk 
+                fs.readFile(tilename, 
+                    function(err, data) {
+                        if(err) {
+                            // Error Condition
+                            res.send('', 404);
+                        } else {
+                            res.writeHead(200, {'Content-Type': 'image/tiff' });
+                            res.end(data, 'binary');
+                        }
+                    }
+                );
                 // Send the tile along
+            } else {
+                // Error Condition
+                res.send('', 404);
             }
 
         });  
-    } 
+    } else {
+        // param Error Condition 
+        // Send a 400 back
+        res.send('', 400);
+    }
 
-    // Error Condition 
-    // Send a 404 back
+
 
 };
 
@@ -63,21 +101,26 @@ exports.rois = function(req, res){
     var width   = req.params.width;
     var length  = req.params.length;
 
-    if(imageId != null) {
+    if(imageId) {
         Image.get(imageId).on('success', function(image) {
-            if(image != null) {
+            if(image) {
                 var filename = image.filename;
+                image.getRois().on('success', function(rois){
                     // Ask the db for all ROIs on a given image
                     // If no bounds are requested, then send ALL ROIs
                     // Else, Get the bounding params
                     // Filter the ROI-set for the bounds
+                });
+                    
             }
 
         });  
-    } 
+    } else {
+        // param Error Condition 
+        // Send a 400 back
+        res.send('', 400);
+    }
 
-    // Error Condition 
-    // Send a 404 back
 
 };
 
@@ -94,11 +137,11 @@ exports.createroi = function(req, res){
     var rLength = req.params.length;
      
     // Ensure that all the right params are passed
-    if(    imageId != null 
-        && xOffset != null 
-        && yOffset != null 
-        && rWidth  != null 
-        && rLength != null) {
+    if(    imageId  
+        && xOffset  
+        && yOffset  
+        && rWidth   
+        && rLength ) {
 
 
             var newRoi = Roi.build({
@@ -111,17 +154,28 @@ exports.createroi = function(req, res){
             // Set the image (it better exist)
             Image.find(imageId).on('success', function(image) {
                 // This will save the ROI automagically
-                if(image != null)
+                if(image) {
                     newRoi.setImage(image);
+                } 
             });
             
+            // Create an ROI instance
+            // commit it to the db
+            // Send a 200OK and the new ID
+            res.send(newRoi.id, 200);
 
+    } else {
+        // param Error Condition 
+        // Send a 400 back
+        res.send('', 400);
     }
 
-    // Create an ROI instance
-    // commit it to the db
-    // Send a 200OK
+};
 
-    // Error Condition 
-    // Send a 404 back
+exports.createtag = function(req, res) {
+    
+};
+
+exports.tagroi = function(req, res) {
+    
 };
