cf7b7c2f938124e709c758b2e38816c373729085
spartango@gmail.com <spartango@gmail.com>
2011-12-28 02:10:16 -0800

    added code for tag creation and destruction

--- routes/tagroute.js
+++ routes/tagroute.js
@@ -14,18 +14,50 @@ Roi.hasMany(Tag);
 
 exports.tags = function(req, res) {
     // Get all the tags from the database
-    // Generate a listing of them
-    // Send it along
+    Tags.findAll().on('success', function(tags){
+        var json = JSON.stringify(tags.map(Tag.stringify));
+        // Generate a listing of them
+        // Send it along
+        res.send(json, 200);
+    });
+
 };
 
 exports.gettag = function(req, res) {
-    // Look up a tag
-    // Get its name
-    // Send that back
+    var tagId = req.params.id;
+    if(tagId) {
+        // Look up a tag
+        Tags.find(Number(tagId)).on('success', function(tag) {
+            if(tag) {
+                // Get its name
+                // Send that back   
+                res.send(tag.name, 200);
+            } else {
+                res.render('404', {title: '404 Bad Tag'});
+            }
+        });
+
+    } else {
+        res.render('404', {title: '404 No Tag'});
+    }
 };
 
 exports.createtag = function(req, res) {
     // Check for appropriate name param
-    // Create the tag
-    // Send back the id
+    var tname = req.params.name;
+    if(tname) {
+        // Create the tag
+        var newTag = Tag.build({
+            name: tname
+        });
+
+        // Send back the id
+        newTag.save().on('success', function(){
+            res.send(newTag.id, 200);
+        })
+
+    } else{
+        res.send('Bad Param', 400);
+    }
+
 };
