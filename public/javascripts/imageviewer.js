2650dfeeda3b75d848dfd7918cf9d3456acf22d0
spartango@gmail.com <spartango@gmail.com>
2012-01-02 19:08:24 -0800

    making progress on the viewer models

--- public/javascripts/imageviewer.js
+++ public/javascripts/imageviewer.js
@@ -1,14 +1,71 @@
-// Blast anything out of the way
-var canvas = null;
-var ctx    = null;
+var ViewedImage = function(name, width, height) {
+    this.xOffset = 0;
+    this.yOffset = 0;
+    this.width   = width;
+    this.height  = height;
+    this.name    = name;
+};
 
-function init() {
-    canvas = document.getElementById('viewport');  
+var Tile = function(x, y, parent) {
+    this.x        = x;
+    this.y        = y
+    this.parent   = parent;
+    this.image    = null;
+    this.getImage = function() {
+        // Generate the image name from the parent
+
+        // Buil & assign image
+    };
+    this.render   = function(context) { 
+        // Check if we have the image
+        if(image) {
+            // Calculate where it ought to be in the canvas
+
+            // Move it to position
+            
+        } 
+    }
+};
+
+var Roi = function() {
+    // TODO 
+};
+
+
+var tileSet = [];
+var roiSet  = [];
+
+function updateImageInfo() {
+    
+}
+
+// Setup Viewport canvas
+function initViewport() {
+
+    window.viewportCanvas = document.getElementById('viewport');  
 
     // Check that these things work ok: 
     if(canvas && canvas.getContext) {
-        ctx = canvas.getContext('2d'); 
-        
-    }    
+        window.viewportContext = canvas.getContext('2d');
+        // Register Events
+         
+        // Setup the canvas with the right images
+
+    } 
 }
 
+function refreshTiles() {
+    
+}
+
+function onViewportMoved() {
+    
+}
+
+function init() {   
+    // Grab info for the target image
+    // Populate the model
+
+    // Init Viewport
+
+}
