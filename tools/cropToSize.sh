10d6d0fa54f199e5551343cf291b187e26796b2a
spartango@gmail.com <spartango@gmail.com>
2012-01-17 12:16:05 -0800

    still polishing minor bugs in ls lol

--- tools/cropToSize.sh
+++ tools/cropToSize.sh
@@ -3,7 +3,7 @@
 TARGET_WIDTH=1024
 TARGET_HEIGHT=1024
 
-for image in `ls ../rawimages/*.png`; do
+for image in `ls ../rawimages/`; do
      # Get the dimensions
      WIDTH=7296
      LENGTH=7200
@@ -11,8 +11,8 @@ for image in `ls ../rawimages/*.png`; do
      for (( xoffset = 0; xoffset < $WIDTH; xoffset+=$TARGET_WIDTH )); do
          for (( yoffset = 0; yoffset < $LENGTH; yoffset+= TARGET_HEIGHT )); do
             PARAMS="$TARGET_WIDTH"x"$TARGET_HEIGHT+$xoffset+$yoffset"
-            echo "Cropping $image with $PARAMS"
-            convert "../images/$image" -crop $PARAMS "../images/$xoffset"_"$yoffset"_"$image"
+            echo "Cropping $image with $PARAMS to ../images/$xoffset"_"$yoffset"_"$image"
+            convert "../rawimages/$image" -crop $PARAMS "../images/$xoffset"_"$yoffset"_"$image"
          done
      done
 done 
