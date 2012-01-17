a5d0b689b06c91df888c93abd4b7fdc4ef4ff632
spartango@gmail.com <spartango@gmail.com>
2012-01-17 12:48:14 -0800

    fixed a stupid imagemagick bug

--- tools/generateTiles.sh
+++ tools/generateTiles.sh
@@ -11,11 +11,11 @@ for image in `ls ../images`; do
      WIDTH=1024
      LENGTH=1024
      # Walk across x and y
-     for (( xoffset = 0; xoffset < $WIDTH; xoffset+=$TILE_WIDTH )); do
-         for (( yoffset = 0; yoffset < $LENGTH; yoffset+= TILE_LENGTH )); do
+     for (( xoffset = 0; xoffset <= WIDTH - TILE_WIDTH; xoffset+= TILE_WIDTH )); do
+         for (( yoffset = 0; yoffset <= LENGTH - TILE_LENGTH; yoffset+= TILE_LENGTH )); do
             PARAMS="$TILE_WIDTH"x"$TILE_LENGTH+$xoffset+$yoffset"
             echo "Cropping $image with $PARAMS"
-            convert "../images/$image" -crop $PARAMS "../tiles/$xoffset"_"$yoffset"_"$image"
+            convert "../images/$image" "+repage" -crop $PARAMS  "../tiles/$xoffset"_"$yoffset"_"$image"
          done
      done
 done 
