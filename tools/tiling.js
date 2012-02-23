f184b6a63649e26c3d0cb51abe389610b613aa6e
spartango@gmail.com <spartango@gmail.com>
2012-02-23 15:23:01 -0500

    added helper, dicer

--- tools/tiling.js
+++ tools/tiling.js
@@ -19,15 +19,31 @@ exports.convertToPng = function (rawPath, targetDir) {
     return targetFile;
 };
 
-exports.cropToSize = function (imagePath, targetDir) {
+function diceImage(imagePath, targetDir, targetWidth, targetHeight) {
     // Identify the image
     imagick.identify(imagePath, function(err, features) {
         var width  = features.width;
         var height = features.height;
         // Raster across image
-        
-    });
+        for(var xOffset = 0; xOffset < width - targetWidth; xOffset += targetWidth) {
+            for (var yOffset = yOffset < height - targetHeight; yOffset += targetHeight) {
+                var parts      = rawPath.split("/");
+                var targetFile = xOffset+"_"+yOffset+"_"+parts[parts.length-1];
 
+                imagick.crop({ srcPath: imagePath, 
+                               dstPath: targetFile,
+                               width:   targetWidth, 
+                               height:  targetHeight,
+                               quality: 1
+                            }, 
+                            function(err, stdout, stderr) {});
+            }
+        }
+
+    });
+}
+
+exports.cropToSize = function (imagePath, targetDir) {
 };
 
 exports.generateThumbs = function(imagePath, targetDir) {
@@ -35,5 +51,5 @@ exports.generateThumbs = function(imagePath, targetDir) {
 };
 
 exports.generateTiles = function(imagePath, targetDir) {
-
+    diceImage(imagePath, targetDir, TILE_WIDTH, TILE_HEIGHT);
 };
